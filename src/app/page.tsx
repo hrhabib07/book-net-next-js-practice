c7fa66fe323a55d070ba224591aea66982bca08f
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-09 21:40:31 +0600

    header and footer section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+
 const HomePage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
+      <Navbar></Navbar>
       <h1 className="text-4xl">Welcome To Book Net Home Page</h1>
+      <Footer></Footer>
     </div>
   );
 };
